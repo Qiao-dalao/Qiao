d8cba542dc1fcacde281085d7b460d7b0cedd40c
song-xiaoyang <508946072@qq.com>
2019-09-17 16:50:15 +0800

    修改recommend

---
+++ 第二阶段作业/js/recommend.js
@@ -0,0 +1,9 @@
+var recommendFind =document.querySelector('.recommend-div-font-p3-span3')
+var recommendTop=document.querySelector('.recommend-top')
+var recommend=document.querySelector('#recommend')
+recommendFind.onclick=function(){
+    console.log('recommendFind')
+    console.log(1)
+    document.display='none'
+    recommend.display="none"
+}
