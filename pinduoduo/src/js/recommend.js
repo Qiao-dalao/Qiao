d90e2ca909b904782ad27faaad4aac15202e6e07
song-xiaoyang <508946072@qq.com>
2019-09-18 17:27:59 +0800

    提交

--- pinduoduo/src/js/recommend.js
+++ pinduoduo/src/js/recommend.js
@@ -1,12 +1,4 @@
-// var recommendFind =document.querySelector('.recommend-div-font-p3-span3')
-// var recommendTop=document.querySelector('.recommend-top')
-// var recommend=document.querySelector('#recommend')
-// recommendFind.onclick=function(){
-//     console.log('recommendFind')
-//     console.log(1)
-//     document.display='none'
-//     recommend.display="none"
-// }
+//回到顶部
 window.onload=function(){
 	var recommendback=document.querySelector('.recommend-back')
 	window.onscroll=function(){
@@ -18,6 +10,57 @@ window.onload=function(){
 	}
 	recommendback.onclick=function(e){
 	    window.scrollTo(0,0)
+	}	
+}
+
+//跳转到发现
+var recommendbottomfill =document.querySelector('#bottom-fill')
+var recommendbottommenu =document.querySelector('.bottom-menu')
+var recommendbaodiv =document.querySelector('.recommend-baodiv')
+var recommendfindreturn=document.querySelector('.recommend-find-return')
+var recommendFind =document.querySelectorAll('.recommend-div-font-p3-span3')
+var recommendfindbaodiv=document.querySelector('.recommend-find-baodiv')
+for(var i=0;i<recommendFind.length-1;i++){
+	recommendFind[i].onclick=function(){
+		console.log(1)
+		recommendfindbaodiv.style.display='block'	
+		recommendbaodiv.style.display="none"
+		recommendbottomfill.style.display='none'
+		recommendbottommenu.style.display='none'
 	}
-	
+}
+recommendfindreturn.onclick =function(){
+	console.log(1)	
+	recommendfindbaodiv.style.display='none'
+	recommendbaodiv.style.display="block"
+	recommendbottomfill.style.display='block'
+	recommendbottommenu.style.display='block'
+}
+//回到顶部
+window.onload=function(){
+	var recommendFindback=document.querySelector('.recommend-find-back')
+	window.onscroll=function(){
+	   if(window.scrollY>100){
+			recommendFindback.style.display='block'
+	   }else{
+			recommendFindback.style.display='none'
+	   }
+	}
+		recommendFindback.onclick=function(e){
+	   	 window.scrollTo(0,0)
+	}	
+}
+//分享
+var recommendfindshare=document.querySelector('.recommend-find-share')
+var recommendfindsharediv=document.querySelector('.recommend-find-share-div')
+var recommendsharediv3=document.querySelector('.recommend-find-share-div3')
+var rzhezaoceng=document.querySelector('.r-zhezaoceng')
+recommendfindshare.onclick =function(){
+	rzhezaoceng.style.display='block'
+	//recommendfindbaodiv.style.display='none'
+	recommendfindsharediv.style.display='block'
+}
+recommendsharediv3.onclick =function(){
+	recommendfindsharediv.style.display='none'
+	rzhezaoceng.style.display='none'
 }
