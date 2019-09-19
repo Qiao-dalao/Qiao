e35bb9744f27c67ff24f406b4cc9157b37a6e38e
Qiao <3530078622@qq.com>
2019-09-19 12:25:46 +0800

    清理

--- 第二阶段作业/js/index/index.js
+++
@@ -1,9 +0,0 @@
-mui.init();
-		
-		var gallery = mui('.mui-slider');
-		gallery.slider({
-			interval:5000
-		});
-		document.querySelector('.mui-slider').addEventListener('slider',function(){
-			console.log("你正在看第" + (event.detail.slideNumber + 1)+ "轮播图")
-		})
