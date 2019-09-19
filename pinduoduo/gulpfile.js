f664b6c69c4600d4b051d483998e9e5f18129be2
Qiao <3530078622@qq.com>
2019-09-19 12:16:27 +0800

    删除

--- pinduoduo/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-
-
-gulp.task('js',function(){
-	return gulp.src('src/js/*js')
-	.pipe(concat('build.js'))
-	.pipe(gulp.dest('dist/js/'))
-})
